6ff8759d9803b64eb1a250eeaa4caeabc3eaa2f0
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-12-08 12:00:05 -0500

    init commit

---
+++ get_events.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+import time
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+
+import json
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def get_events():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds:
+        print("no creds")
+
+    if not creds.valid:
+        print("no creds valid")
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        # print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='kim6n4bbnpecd1cm165mmej9e10hkdju@import.calendar.google.com', timeMin=now,
+                                               singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        print(len(events))
+        for event in events:
+            # print(event["start"])
+            if("Schedule" in event["summary"]):
+                if(event["start"]["date"] == str(datetime.date.today())):
+                    print("there is school")
+                # else:
+                    # print("there is not school today, todays date: " + str(datetime.date.today()) + " the events date "+ event["start"]["date"])
+            # start = event['start'].get('dateTime', event['start'].get('date'))
+            # print(start, event['summary'])
+
+        return events
+        # calendar = service.calendars().get(calendarId='kim6n4bbnpecd1cm165mmej9e10hkdju@import.calendar.google.com').execute()
+
+        # print(calendar)
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+file = open("Schedule.json", "w")
+file.write(json.dumps(get_events()))
+file.close()
---
+++ main copy.py
@@ -0,0 +1,104 @@
+"""
+
+
+*******************
+WARNING
+when transfering to raspberry pi change the location of credentials.json
+
+*******************
+
+
+
+
+"""
+from __future__ import print_function
+
+import datetime
+import os.path
+import time
+
+
+
+
+import json
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+
+school_timings = ""
+
+def count_later_seconds(todays_day_type):
+    seconds_remaining_today = 0
+
+    for period in range(0, len(school_timings[todays_day_type])):
+        school_timings[todays_day_type][period]["start"][0]
+        start_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, school_timings[todays_day_type][period]["start"][0], school_timings[todays_day_type][period]["start"][1])
+        end_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, school_timings[todays_day_type][period]["end"][0], school_timings[todays_day_type][period]["end"][1])
+        seconds_remaining_today += time.mktime(end_time.timetuple()) - time.mktime(start_time.timetuple())
+    return seconds_remaining_today
+
+
+
+def count_today_seconds(todays_day_type):
+    seconds_remaining_today = 0
+
+    for period in range(0, len(school_timings[todays_day_type])):
+        school_timings[todays_day_type][period]["start"][0]
+        start_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, school_timings[todays_day_type][period]["start"][0], school_timings[todays_day_type][period]["start"][1])
+        end_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, school_timings[todays_day_type][period]["end"][0], school_timings[todays_day_type][period]["end"][1])
+        current_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, datetime.datetime.now().hour, datetime.datetime.now().minute, datetime.datetime.now().second)
+        
+        if(current_time > start_time and not current_time > end_time):
+            seconds_remaining_today += time.mktime(end_time.timetuple()) - time.mktime(current_time.timetuple())
+        elif(not current_time > end_time):
+            seconds_remaining_today += time.mktime(end_time.timetuple()) - time.mktime(start_time.timetuple())
+    return seconds_remaining_today
+
+
+
+
+def main():
+    global school_timings
+    file = open("school.json", "r")
+    string = file.read()
+    # print(string)
+    school_timings = json.loads(string)
+    file.close()
+
+    file = open("Schedule.json", "r")
+    schedule_string = file.read()
+    schedule = json.loads(schedule_string)
+    file.close()
+
+    # print(type(schedule))
+
+    later_day_seconds = 0
+
+    for event in schedule:
+        if("Schedule" in event["summary"]):
+            # if(event["start"]["date"] == str(datetime.date.today())):
+            #     today_seconds += today_seconds(event["summary"][0])
+            if(event["start"]["date"] > str(datetime.date.today()) and event["start"]["date"] < str(datetime.datetime(2022, 12, 12))):
+                later_day_seconds += count_later_seconds(event["summary"][0])
+    
+
+    todays_day_type = ""
+    today_seconds = 0
+    for event in schedule:
+        if("Schedule" in event["summary"]):
+            if(event["start"]["date"] == str(datetime.date.today())):
+                today_seconds += count_today_seconds(event["summary"][0])
+                todays_day_type = event["summary"][0]
+            # elif(event["start"]["date"] > str(datetime.date.today())):
+            #     later_day_seconds += later_seconds(event["summary"][0])
+    total_seconds = later_day_seconds + today_seconds
+    print(f"{total_seconds}s in this semester, {today_seconds}s in this {todays_day_type} day, and the time is " + datetime.datetime.now().strftime("%I:%M:%S %p"))
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
